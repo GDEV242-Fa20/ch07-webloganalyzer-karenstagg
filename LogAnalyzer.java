513dcbd9212d90204a5d403385e34c4e3c16c54c
karenstagg <42982417+karenstagg@users.noreply.github.com>
2020-10-17 18:11:03 -0400

    new overloaded constructor, numberOfAccesses methode, create a demoLog.txt file and
    create overloaded constructor accepting filename, create a demoLog.txt file and add numberOfAccesses method and test using my created demoLog.txt file

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -1,8 +1,9 @@
 /**
  * Read web server data and analyse hourly access patterns.
+ * Supplying your own filename is an added feature.
  * 
- * @author David J. Barnes and Michael Kölling.
- * @version    2016.02.29
+ * @author Karen Stagg
+ * @version October 19,2020
  */
 public class LogAnalyzer
 {
@@ -12,7 +13,9 @@ public class LogAnalyzer
     private LogfileReader reader;
 
     /**
-     * Create an object to analyze hourly web accesses.
+     * Constructor
+     * Create an object to analyze hourly web accesses. A default filename 
+     * of demo.log will be ther reader name.
      */
     public LogAnalyzer()
     { 
@@ -20,8 +23,23 @@ public class LogAnalyzer
         // access counts.
         hourCounts = new int[24];
         // Create the reader to obtain the data.
-        reader = new LogfileReader("demo.log");
+        reader = new LogfileReader();
     }
+   
+    /**
+     * Overlaoded Constructor to add your own filemane instead of using default.
+     * Create an object to analyze hourly web accesses.
+     * @param String namedFile is a filename given by the user to create a new 
+     * reader with a supplied name insted of the default "demo.log".
+     */
+    public LogAnalyzer(String filename)
+    { 
+        // Create the array object to hold the hourly
+        // access counts.
+        hourCounts = new int[24];
+        // Create the reader to obtain the data.
+        reader = new LogfileReader(filename);
+    } 
 
     /**
      * Analyze the hourly access data from the log file.
@@ -33,6 +51,24 @@ public class LogAnalyzer
             int hour = entry.getHour();
             hourCounts[hour]++;
         }
+    }
+    
+    /**
+     * Return the number of accesses recorded in the log file.
+     * @return total is the int value of the number of accesses recorded 
+     * in the log file.
+     */
+    public int numberOfAccesses()
+    {
+        int total = 0;
+        //Add the value in each element of hourCounts to total.
+        /*for (int index = 0; index < hourCounts.length; index++)
+        {
+            total += hourCounts[index];
+        }*/
+        for (int hourCount:hourCounts)
+            total = total + hourCount;
+        return total;
     }
 
     /**
